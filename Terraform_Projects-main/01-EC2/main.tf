c78a35a9f99ff6a0f89a6d5f936df49783dec606
E Njang <104914526+Ekangaki@users.noreply.github.com>
2023-12-19 03:46:31 -0500

    Update main.tf

--- Terraform_Projects-main/01-EC2/main.tf
+++ Terraform_Projects-main/01-EC2/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "linux-vm"{	
-	ami = "ami-02e94b011299ef128"
+	ami = "ami-0230bd60aa48260c6"
 	instance_type = "t2.micro"
-	key_name = "ashokitkeypair"
+	key_name = "Batch5-keypair"
 	security_groups = ["default"]
 	tags = {
-		Name = "AshokIT-Linux-VM"
+		Name = "ekangaki-Linux-VM"
 	}
 }
